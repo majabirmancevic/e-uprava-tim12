0f4233977e602fee495dec423a607efe9539dc50
Nole35 <lazic.sr9.2020@uns.ac.rs>
2024-01-18 14:26:42 +0100

    dovrsen serverski dio zadatka

--- studentski-dom/src/main/resources/data.sql
+++ studentski-dom/src/main/resources/data.sql
@@ -1,10 +1,24 @@
-INSERT INTO student (username, password, ime, prezime, jmbg, godinaStudiranja, osvojeniBodovi, prosek, bodovi, kartica, soba_id) VALUES
-('maja123', 'maja123', 'Marko', 'Marković', '1234567890123', 2, 120, 8.5, 150.0, 'BUDZET', 1),
-('student2', 'password2', 'Ana', 'Anić', '9876543210123', 3, 90, 7.0, 100.0, 'BUDZET', 2);
+INSERT INTO dom (id, naziv) VALUES
+(1, 'Dom A'),
+(2, 'Dom B'),
+(3, 'Dom C');
 
-INSERT INTO upravnik (username, password, ime, prezime, jmbg) VALUES
-('admin', 'admin123', 'Admin', 'Adminović', '1111222233334');
+INSERT INTO konkurs (id, grad, skolska_godina) VALUES
+(1, 'Beograd', '2024/2025'),
+(2, 'Novi Sad', '2024/2025');
 
-INSERT INTO konkurs (grad, skolska_godina) VALUES
-('Beograd', '2024/2025'),
-('Novi Sad', '2024/2025');
+
+INSERT INTO soba (id, broj_sobe, dom_id) VALUES
+(1, '101', 1),
+(2, '201', 2),
+(3, '301', 3);
+
+
+INSERT INTO student (id, username, password, ime, prezime, jmbg, godina_studiranja, osvojeni_bodovi, prosek, bodovi, kartica, soba_id) VALUES
+(1, 'maja123', 'maja123', 'Marko', 'Marković', '1234567890123', 2, 120, 8.5, 150.0, 'BUDZET', 1),
+(2, 'student2', 'password2', 'Ana', 'Anić', '9876543210123', 3, 90, 7.0, 100.0, 'BUDZET', 2);
+
+
+INSERT INTO upravnik (id, username, password, ime, prezime, jmbg) VALUES
+(1, 'admin', 'admin123', 'Admin', 'Adminović', '1111222233334');
+
